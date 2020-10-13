988ae1cb4e85820dd85daf84501e220e39073ffd
vik-addweb <vikrant.addweb@gmail.com>
2020-10-13 19:30:12 +0530

    Added Multimedia Gallery Component

---
+++ src/components/MultimediaGallery/Lightbox/icons/index.js
@@ -0,0 +1,11 @@
+'use strict';
+import arrowLeft from './arrowLeft';
+import arrowRight from './arrowRight';
+import close from './close';
+import download from './download';
+export default {
+	arrowLeft,
+	arrowRight,
+	close,
+	download
+};
