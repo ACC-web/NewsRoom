59356ec907ebfa80e6be2d72eeb67446ec1165e1
RickyACC <rickyhobbs@acc.edu.au>
2020-10-14 16:30:25 +1100

    various changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,15 +19,13 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 // Import Swiper styles
 import 'swiper/swiper-bundle.css'
 
-// import SwiperComponent from "swiper";
-
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin: 1rem 1rem 0 1rem;
 
   @media (min-width: 768px) {
-    flex-direction: row;
+    flex-direction: column;
     margin: 3rem 1rem 0 1rem;
   }
 `
@@ -43,6 +41,42 @@ const BackgroundImage = styled(Img)`
   right: 0;
   
   background-color: #023e88;
+`
+
+const HomeTop = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  > div{
+    //width: 50%;
+    display: flex;
+    flex-direction: column;
+      
+      &:nth-child(1){
+      flex-grow: 1;
+      }
+      &:nth-child(2){
+        flex-grow: 1;
+        min-width: unset;
+        margin-left: unset;
+      }
+  }
+  
+  @media(min-width: 768px){
+      flex-direction: row;
+      
+      >div{
+        display: flex;
+          &:nth-child(1){
+            flex: 1 1 66%
+          }
+          &:nth-child(2){
+            flex: 2 2 33%;
+            min-width: 300px;
+            margin-left: 1rem;
+          }
+      }
+  }
 `
 
 const SectionHeading = styled.h2`
@@ -190,67 +224,69 @@ class RootIndex extends React.Component {
 
             <Wrapper className="wrapper">
 
-              <h2 style={{ fontSize: '1.8rem', fontWeight: '500' }}>Newsroom</h2>
-              <p style={{ marginTop: '0' }}>
-                  This newsroom exists to help the media access facts, stories, comments, information, photographs and videos about our schools. Journalists and media representatives can also request school visits and expert comments.
-              </p>
-              <p style={{ }}>
-                  This newsroom includes a logo, photo and video gallery, infographics for use by the media, a live stream of media releases and story starters.
-              </p>
+                <HomeTop>
+                    <div>
+                      <h2 style={{ fontSize: '1.8rem', fontWeight: '500' }}>Newsroom</h2>
+                      <p style={{ marginTop: '0' }}>
+                          This newsroom exists to help the media access facts, stories, comments, information, photographs and videos about our schools. Journalists and media representatives can also request school visits and expert comments.
+                      </p>
+                      <p>
+                          This newsroom includes a logo, photo and video gallery, infographics for use by the media, a live stream of media releases and story starters.
+                      </p>
+                    </div>
+                    <MediaContainer>
+                        <h2 style={{ fontSize: '1.1rem', marginBottom: '1.47rem' }}>Media Contacts</h2>
+                        {mediacontact.map(({ node }) => {
+                            return <MediaContacts key={node.id} contact={node} />
+                        })}
+                    </MediaContainer>
+                </HomeTop>
+                <section>
+                <h2 className="section-headline">Story Starters</h2>
+                    <Accordion>
+                        {story.map(({ node }) => {
+                            return (
+                                <div key={node.slug} label={node.title}>
+                                        <span className="accordionContent">
+                                            <span dangerouslySetInnerHTML={{
+                                                    __html: node.body.childMarkdownRemark.html
+                                                }}
+                                            />
+                                        </span>
+                                </div>
+                            )
+                        })}
 
-                <MediaContainer>
-                    <h2 style={{ fontSize: '1.1rem', marginBottom: '1.47rem'}}>Media Contacts</h2>
-                    {mediacontact.map(({ node }) => {
-                        return <MediaContacts key={node.id} contact={node} />
-                    })}
-                </MediaContainer>
-            <section>
-            <h2 className="section-headline">Story Starters</h2>
-                <Accordion>
-                    {story.map(({ node }) => {
-                        return (
-                            <div key={node.slug} label={node.title}>
-                                    <span className="accordionContent">
-                                        <a className="imageLink" href={node.image.file.url}><img src={node.image.fluid.src} /></a>
-                                        <span dangerouslySetInnerHTML={{
-                                                __html: node.body.childMarkdownRemark.html
-                                            }}
-                                        />
-                                    </span>
-                            </div>
-                        )
-                    })}
+                    </Accordion>
+              </section>
+              <hr />
+              <h4 style={{ textAlign: 'center', fontWeight: '400', textTransform: 'uppercase' }}>Australian Christian College has received media coverage in</h4>
+              <Swiper
+                  spaceBetween={50}
+                  slidesPerView={4}
+                  onSlideChange={() => console.log('slide change')}
+                  onSwiper={(swiper) => console.log(swiper)}
+                  navigation
+                  pagination={{ clickable: true }}
+                  scrollbar={{ draggable: true }}
+                  autoPlay={1}
 
-                </Accordion>
-          </section>
-          <hr />
-          <h4 style={{ textAlign: 'center', fontWeight: '400', textTransform: 'uppercase' }}>Australian Christian College has received media coverage in</h4>
-          <Swiper
-              spaceBetween={50}
-              slidesPerView={4}
-              onSlideChange={() => console.log('slide change')}
-              onSwiper={(swiper) => console.log(swiper)}
-              navigation
-              pagination={{ clickable: true }}
-              scrollbar={{ draggable: true }}
-              autoPlay={1}
+                  style={{ display: 'flex', alignItems: 'center' }}
+              >
+                  {logoSlide.map(({ node }) => {
+                      return (
+                          <SwiperSlide key={node.id}>
+                              <Logo fluid={node.childImageSharp.fluid} style={{ width: '100%' }} objectFit="contain" alt="Media Outlets" objectPosition="50% 50%" />
+                          </SwiperSlide>
+                      )
+                  })}
+              </Swiper>
+              {/*<Img fluid={medialogos} objectFit="cover" alt="Media Outlets" objectPosition="50% 50%" />*/}
+                <VisitForm />
 
-              style={{ display: 'flex', alignItems: 'center' }}
-          >
-              {logoSlide.map(({ node }) => {
-                  return (
-                      <SwiperSlide key={node.id}>
-                          <Logo fluid={node.childImageSharp.fluid} style={{ width: '100%' }} objectFit="contain" alt="Media Outlets" objectPosition="50% 50%" />
-                      </SwiperSlide>
-                  )
-              })}
-          </Swiper>
-          {/*<Img fluid={medialogos} objectFit="cover" alt="Media Outlets" objectPosition="50% 50%" />*/}
-            <VisitForm />
-
-            {/*<Sidebar>*/}
-                {/*<MediaAssets />*/}
-            {/*</Sidebar>*/}
+                {/*<Sidebar>*/}
+                    {/*<MediaAssets />*/}
+                {/*</Sidebar>*/}
           </Wrapper>
         </div>
       </Layout>
