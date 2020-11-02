1ee11becf1eaa783167b29ae927ab97467615a6d
ACC-Ricky <58411863+ACC-Ricky@users.noreply.github.com>
2020-11-03 10:26:24 +1100

    Update Gallery.js

--- src/components/MultimediaGallery/Gallery.js
+++ src/components/MultimediaGallery/Gallery.js
@@ -19,7 +19,8 @@ const LoaderWrapper = styled.div`
 `;
 const Image = styled.img`
   display: block;
-  border: 1px solid #f3f3f6;
+  // RH I removed this border because the images were too wide by 1 pixel for some reason, not sure why
+  //border: 1px solid #f3f3f6;
 `;
 
 const Caption = styled.p`
